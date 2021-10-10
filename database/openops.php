7a7ccc4f7f7d4e86407c8b6213cb85a850e63c1c
Abhay Lad <64049217+abhaylad14@users.noreply.github.com>
2021-10-10 08:47:00 +0530

    Update openops.php

--- database/openops.php
+++ database/openops.php
@@ -129,12 +129,12 @@ function sendEmail($email, $subject, $message) {
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth = true;                                   // Enable SMTP authentication
-    $mail->Username = 'ladaebs14@gmail.com';                     // SMTP username
-    $mail->Password = 'Abhay@123';                               // SMTP password
+    $mail->Username = '<your email ID>';                     // SMTP username
+    $mail->Password = '<Password>';                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
     //Recipients
-    $mail->setFrom('ladaebs14@gmail.com', 'Abhay');
+    $mail->setFrom('<Your email ID>', '<Name of sender>');
     $mail->addAddress($email);
     $mail->isHTML(true);
     $mail->Subject = $subject;
